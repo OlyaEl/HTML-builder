463d5437520bf06e22a73e3c58bf425bd8fa7da7
OlyaEl <128423432+OlyaEl@users.noreply.github.com>
2025-01-20 17:12:20 +0300

    feat: add solution of 02 Writing Console Input to File

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+const { stdout, stdin } = process;
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
+
+const createPathFile = path.join(__dirname, 'text.txt');
+const writingStream = fs.createWriteStream(createPathFile)
+
+const rl = readline.createInterface({
+  input: stdin,
+  output: stdout,
+});
+
+const writeMessage = (message) => {
+  stdout.write(message)
+  rl.on('line', (line) => {
+    if (line.trim() === 'exit') {
+      rl.close();
+    } else {
+      writingStream.write(line + '\n');
+      stdout.write(message);
+    }
+})
+rl.on('close', () => {
+  stdout.write('GOOD BYE! ^_^');
+  writingStream.close();
+  process.exit(0)
+})
+rl.on('SIGINT', () => {
+  rl.close();
+});
+}
+writeMessage('Write your message, please.\n')
